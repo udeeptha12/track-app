6b74d3526e8aed3147b0266e53690714d5c88eb3
udeeduino <udeeduino@gmail.com>
2023-08-11 00:37:14 +0530

    track-app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShareModule } from './modules/share/share.module';
+import { NotFoundComponent } from './core/not-found/not-found.component';
+import { MainFooterComponent } from './core/main-footer/main-footer.component';
+import {MatInputModule} from "@angular/material/input";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AngularFireModule} from "@angular/fire/compat";
+import {AngularFireAuthModule} from "@angular/fire/compat/auth";
+import {AngularFireStorageModule} from "@angular/fire/compat/storage";
+import {AngularFirestoreModule} from "@angular/fire/compat/firestore";
+import {AngularFireDatabaseModule} from "@angular/fire/compat/database";
+import { environmentProd } from '../environments/environment.prod';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
+    MainFooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ShareModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environmentProd.firebase),
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    AngularFirestoreModule,
+    AngularFireDatabaseModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
